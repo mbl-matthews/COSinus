374f09ae192a3990b835bd06c62b0f9ded8a635f
mbl <mbl.85@web.de>
2021-09-08 23:08:16 +0200

    Added Navigation Drawer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,6 +5,7 @@
       color="primary"
       dark
     >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -26,6 +27,46 @@
         <v-icon>mdi-logout</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+    >
+      <v-list
+          nav
+          dense
+      >
+        <v-list-item>
+          <img
+              :src="require('./assets/cosinus_logo_little_white.png')"
+              width="200"
+          >
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              :to="item.route"
+              link
+              color="#34924f"
+          >
+            <v-list-item-icon>
+              <v-icon color="#c3ccd9">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title class="greyTxt">{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view/>
@@ -39,7 +80,13 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
+    items: [
+      {title: 'Home', icon: 'mdi-home', route: '/'},
+      {title: 'Cash Register', icon: 'mdi-account-cash', route: '/cashregister'},
+      {title: 'About', icon: 'mdi-account', route: '/about'},
+    ]
   }),
 };
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import CashRegister from "@/views/CashRegister";
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: "/cashregister",
+    name: "Cash Register",
+    component: CashRegister
   },
   {
     path: '/about',
