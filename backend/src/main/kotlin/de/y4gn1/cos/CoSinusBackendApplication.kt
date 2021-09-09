4b5396e3cc9e83fc8d7cb0b9e2e1ec498db2287f
Mathis B <mbl.85@web.de>
2021-09-09 11:16:11 +0200

    Added spring boot initializer project

---
+++ backend/src/main/kotlin/de/y4gn1/cos/CoSinusBackendApplication.kt
@@ -0,0 +1,11 @@
+package de.y4gn1.cos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoSinusBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoSinusBackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/de/y4gn1/cos/CoSinusBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.y4gn1.cos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoSinusBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
